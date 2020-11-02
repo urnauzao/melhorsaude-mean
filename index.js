0a0182ed57e585ff947c7e9a23b3f2e751194401
urnauzao <julioc_808@hotmail.com>
2020-11-02 10:23:10 -0300

    -

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
-const medicos = require('./medicos');
+const mongoose = require('mongoose');
+
+//Database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/melhor_saude_mean', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB conectado"))
+.catch((err)  =>  console.error("Erro ao  conectar  com  o MongoDB "+err))
+
+//Models
+const Medico = require('./medicos');
 
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-function requestListener(request, response){
-    if(request.url === "/medicos"){
-        response.writeHead(200, {"Content-Type" : "application/json"});
-        response.write(JSON.stringify(medicos));
-        response.end();
-    }    
-}
+// function requestListener(request, response){
+//     if(request.url === "/medicos"){
+//         response.writeHead(200, {"Content-Type" : "application/json"});
+//         response.write(JSON.stringify(medicos));
+//         response.end();
+//     }    
+// }
 
 /** @method Post */
 app.post('/medico', function(req,res){
@@ -21,19 +33,29 @@ app.post('/medico', function(req,res){
         res.sendStatus(400);
     }
 
-    medicos.create(data.clinica_id,data.nome,data.idade,data.especializacao,data.preco_consulta,data.telefone,data.email,data.whatsapp,data.foto);
-    res.sendStatus(201);
+    // medicos.create(data.clinica_id,data.nome,data.idade,data.especializacao,data.preco_consulta,data.telefone,data.email,data.whatsapp,data.foto);
+    // res.sendStatus(201);
+
+    new Medico(data).save()
+        .then(() => res.sendStatus(201))
+        .catch(() => res.sendStatus(400))
 })
 
 /** @method GetAll */
 app.get('/medicos', function(req,res){
-    res.send(medicos.getAll());
+    // res.send(medicos.getAll());
+    Medico.find()
+        .then((medicos) => res.send(medicos))
+        .catch(() => res.sendStatus(400));
 });
 
 /** @method GetOne */
 app.get('/medico/:index', function(req,res){
     const indexData = req.params.index;
-    res.send(medicos.getOne(indexData));
+    // res.send(medicos.getOne(indexData));
+    Medico.findById(indexData)
+        .then((medico) => res.send(medico))
+        .catch(() => res.sendStatus(400));
 });
 
 /** @method PUT */
@@ -44,8 +66,11 @@ app.put('/medico/:index', function(req,res){
         res.sendStatus(400);
     }
 
-    medicos.update(data.clinica_id,data.nome,data.idade,data.especializacao,data.preco_consulta,data.telefone,data.email,data.whatsapp,data.foto, indexData);
-    res.sendStatus(200);
+    // medicos.update(data.clinica_id,data.nome,data.idade,data.especializacao,data.preco_consulta,data.telefone,data.email,data.whatsapp,data.foto, indexData);
+    // res.sendStatus(200);
+    Medico.findByIdAndUpdate(indexData, data)
+        .then(( ) => res.sendStatus(200))
+        .catch(() => res.sendStatus(400));
 });
 
 /** @method Delete */
@@ -54,8 +79,12 @@ app.delete('/medico/:index', function (req, res){
     if(!data || !indexData){
         res.sendStatus(400);
     }
-    filmes.delete(indexData);
-    res.sendStatus(200);
+    // filmes.delete(indexData);
+    // res.sendStatus(200);
+
+    Medico.findByIdAndRemove(indexData)
+        .then(() => res.sendStatus(200))
+        .catch(() => res.sendStatus(400));
 });
 
 app.listen(8000, () => {
--- medicos.js
+++ medicos.js
@@ -1,104 +1,136 @@
-class Medico {
-    constructor(
-        clinica_id,
-        nome,
-        idade,
-        especializacao,
-        preco_consulta,
-        telefone,
-        email,
-        whatsapp,
-        foto
-    ){
-        this.clinica_id = clinica_id;
-        this.nome = nome;
-        this.idade = idade;
-        this.especializacao = especializacao;
-        this.preco_consulta = preco_consulta;
-        this.telefone = telefone;
-        this.email = email;
-        this.whatsapp = whatsapp;
-        this.foto = foto;
+
+const mongoose = require('mongoose');
+const MedicoSchema = new mongoose.Schema({
+    clinica_id: {
+        type: String,
+        required: false
+    },
+    nome: {
+        type: String,
+        required: true
+    },
+    idade: {
+        type: Number,
+        required: true
+    },
+    especializacao: {
+        type: String,
+        required: true
+    },
+    preco_consulta: {
+        type: Number,
+        required: true
+    },
+    telefone: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    whatsapp: {
+        type: Number,
+    },
+    foto: {
+        type: String,
     }
-}
+});
+mongoose.model('medicos', MedicoSchema);
+module.exports = mongoose.model('medicos');
 
-const medicos = [
-    new Medico(
-        "1",
-        "Zack Mounsey",
-        "64",
-        "Northstar RxLLC",
-        "$105.56",
-        "+353 (969) 563-5095",
-        "zmounsey0@de.vu",
-        "6069277643",
-        "http://dummyimage.com/250x250.jpg/dddddd/000000"
-    ), 
-    new Medico(
-        "2",
-        "Lorain Robilart",
-        "38",
-        "IVAX Pharmaceuticals, Inc.",
-        "$279.40",
-        "+370 (623) 174-2258",
-        "lrobilart1@yellowpages.com",
-        "2269893208",
-        "http://dummyimage.com/250x250.jpg/dddddd/000000"
-    ), 
-    new Medico(
-        "3",
-        "Wilhelmina Saur",
-        "53",
-        "Cadila Healthcare Limited",
-        "$825.27",
-        "+55 (267) 526-2638",
-        "wsaur2@macromedia.com",
-        "7967345731",
-        "http://dummyimage.com/250x250.jpg/cc0000/ffffff"
-    ), 
-    new Medico(
-        "4",
-        "Shem Chomiszewski",
-        "26",
-        "Derma Sciences Canada, Inc.",
-        "$208.88",
-        "+62 (244) 232-8187",
-        "schomiszewski3@twitpic.com",
-        "6488840712",
-        "http://dummyimage.com/250x250.jpg/dddddd/000000"
-    )
-];
 
-function getAll(){
-    return medicos;
-}
+// class Medico {
+//     constructor(
 
-function getOne(index){
-    return medicos[index];
-}
+//     ){
+//         this.clinica_id = clinica_id;
+//         this.nome = nome;
+//         this.idade = idade;
+//         this.especializacao = especializacao;
+//         this.preco_consulta = preco_consulta;
+//         this.telefone = telefone;
+//         this.email = email;
+//         this.whatsapp = whatsapp;
+//         this.foto = foto;
+//     }
+// }
 
-function create(clinica_id,nome,idade,especializacao,preco_consulta,telefone,email,whatsapp,foto){
-    medicos.push(new Medico(clinica_id,nome,idade,especializacao,preco_consulta,telefone,email,whatsapp,foto))
-}
+// const medicos = [
+//     new Medico(
+//         "1",
+//         "Zack Mounsey",
+//         "64",
+//         "Northstar RxLLC",
+//         "$105.56",
+//         "+353 (969) 563-5095",
+//         "zmounsey0@de.vu",
+//         "6069277643",
+//         "http://dummyimage.com/250x250.jpg/dddddd/000000"
+//     ), 
+//     new Medico(
+//         "2",
+//         "Lorain Robilart",
+//         "38",
+//         "IVAX Pharmaceuticals, Inc.",
+//         "$279.40",
+//         "+370 (623) 174-2258",
+//         "lrobilart1@yellowpages.com",
+//         "2269893208",
+//         "http://dummyimage.com/250x250.jpg/dddddd/000000"
+//     ), 
+//     new Medico(
+//         "3",
+//         "Wilhelmina Saur",
+//         "53",
+//         "Cadila Healthcare Limited",
+//         "$825.27",
+//         "+55 (267) 526-2638",
+//         "wsaur2@macromedia.com",
+//         "7967345731",
+//         "http://dummyimage.com/250x250.jpg/cc0000/ffffff"
+//     ), 
+//     new Medico(
+//         "4",
+//         "Shem Chomiszewski",
+//         "26",
+//         "Derma Sciences Canada, Inc.",
+//         "$208.88",
+//         "+62 (244) 232-8187",
+//         "schomiszewski3@twitpic.com",
+//         "6488840712",
+//         "http://dummyimage.com/250x250.jpg/dddddd/000000"
+//     )
+// ];
 
-function update(clinica_id,nome,idade,especializacao,preco_consulta,telefone,email,whatsapp,foto,indexUpdate){
-    let medicoObject = new Medico(clinica_id,nome,idade,especializacao,preco_consulta,telefone,email,whatsapp,foto);
+// function getAll(){
+//     return medicos;
+// }
 
-    medicos = medicos.map((medico,index) => {
-        if(index === indexUpdate){
-            return medicoObject;
-        }
-        return medico;
-    });
-}
+// function getOne(index){
+//     return medicos[index];
+// }
 
-function deleteMedico(indexDelete){
-    medicos.splice(indexDelete, 1);
-}
+// function create(clinica_id,nome,idade,especializacao,preco_consulta,telefone,email,whatsapp,foto){
+//     medicos.push(new Medico(clinica_id,nome,idade,especializacao,preco_consulta,telefone,email,whatsapp,foto))
+// }
 
-module.exports.medicos = medicos;
-module.exports.getAll = getAll;
-module.exports.getOne = getOne;
-module.exports.create = create;
-module.exports.update = update;
-module.exports.delete = deleteMedico;
+// function update(clinica_id,nome,idade,especializacao,preco_consulta,telefone,email,whatsapp,foto,indexUpdate){
+//     let medicoObject = new Medico(clinica_id,nome,idade,especializacao,preco_consulta,telefone,email,whatsapp,foto);
+
+//     medicos = medicos.map((medico,index) => {
+//         if(index === indexUpdate){
+//             return medicoObject;
+//         }
+//         return medico;
+//     });
+// }
+
+// function deleteMedico(indexDelete){
+//     medicos.splice(indexDelete, 1);
+// }
+
+// module.exports.medicos = medicos;
+// module.exports.getAll = getAll;
+// module.exports.getOne = getOne;
+// module.exports.create = create;
+// module.exports.update = update;
+// module.exports.delete = deleteMedico;
